62f47ce0feafbc01b299bc33a82c54859ea5cd69
PIYAL\m_m_a <asif.ferdous@mobileone.com.my>
2024-08-21 22:45:43 +0800

    ddfgdfg

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ import { tasksReducer } from './app/tasks/state/tasks.reducer';
 import { dashboardReducer } from './app/dashboard/state/dashboard.reducer';
 import { TasksEffects } from './app/tasks/state/tasks.effects';
 import { DashboardEffects } from './app/dashboard/state/dashboard.effects';
-
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { StoreModule } from '@ngrx/store';
 bootstrapApplication(AppComponent, {
   providers: [
     provideStore({ tasks: tasksReducer, dashboard: dashboardReducer }),
     provideEffects([TasksEffects, DashboardEffects]),
-  ],
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideRouter(routes),
+],
 }).catch((err) => console.error(err));
